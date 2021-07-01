16c63bc4d5a30b247c2ea5b3c0538f0df8c352e4
Michael Keith <michaelkeith@utah.gov>
2021-07-01 14:26:19 -0600

    Trying a new thing
    Could not get my package to import, so trying something else

--- scalecast/__init__.py
+++
@@ -1,1 +0,0 @@
-#from scalecast.Forecaster import Forecaster
--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ setup(
   packages = ['Scalecast'],
   version = '0.1.2',
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A flexible, minimal-code forecasting object meant to be used with loops to forecast many series or to focus on one series for maximum accuracy.',   # Give a short description about your library
   author = 'Michael Keith',
   author_email = 'mikekeith52@gmail.com',
   url = 'https://github.com/mikekeith52/scalecast',
-  download_url = 'https://github.com/mikekeith52/scalecast/archive/refs/tags/0.1.2.tar.gz',
   keywords = ['FORECAST', 'SCALE', 'FLEXIBLE'],
-  classifiers=[
-    'Development Status :: 4 - Beta',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
-    'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
+  install_requires = [
+    'scikit-learn',
+    'statsmodels',
+    'eli5',
+    'numpy',
+    'pandas',
+    'scipy',
+    'matplotlib',
+    'seaborn',
   ],
 )
