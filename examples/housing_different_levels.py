7b6717d626c7db3b874c36b883aca702d6664539
Michael Keith <michaelkeith@utah.gov>
2021-07-10 12:40:21 -0600

    New Dist
    Reran examples, new tar file, new images in assets

--- examples/housing_different_levels.py
+++ examples/housing_different_levels.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import pandas_datareader as pdr
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-from scalecast.Forecaster import Forecaster
+from scalecastdev.Forecaster import Forecaster
 
 df = pdr.get_data_fred('HOUSTNSA',start='1900-01-01',end='2021-05-01')
 f = Forecaster(y=df['HOUSTNSA'],current_dates=df.index) # to initialize, specify y and current_dates (must be arrays of the same length)
@@ -22,8 +22,8 @@ f.add_poly_terms('t',pwr=3) # by default, creates an order 2 regressor, n-order 
 f.set_validation_length(6) # length, different than test_length, to tune the hyperparameters 
 
 # automatically tune and forecast with a series of models
-models = ('mlr','knn','svr','xgboost','gbt','elasticnet','mlp')
-level_models = ('arima','hwes','prophet')
+models = ('knn','svr','lightgbm','mlp')
+level_models = ('arima','hwes','prophet','silverkite')
 for m in models:
 	f.set_estimator(m)
 	f.tune() # by default, will pull the grid with the same name as the estimator (mlr will pull the mlr grid, etc.)
@@ -31,8 +31,8 @@ for m in models:
 
 # combine models and run manually specified models of other varieties
 f.set_estimator('combo')
-f.manual_forecast(how='simple',models='top_3',determine_best_by='ValidationMetricValue',call_me='avg') # simple average of top_3 models based on performance in validation
-f.manual_forecast(how='weighted',models=models,determine_best_by='ValidationMetricValue',call_me='weighted') # weighted average of all models based on metric specified in determine_best_by (default is the validation metric)
+f.manual_forecast(how='simple',models=models,determine_best_by='ValidationMetricValue',call_me='avg_diff') # simple average of top_3 models based on performance in validation
+f.manual_forecast(how='weighted',models=models,determine_best_by='ValidationMetricValue',call_me='weighted_diff') # weighted average of all models based on metric specified in determine_best_by (default is the validation metric)
 
 f.undiff()
 for m in level_models:
@@ -45,9 +45,10 @@ f.set_estimator('combo')
 f.manual_forecast(how='simple',models=level_models,determine_best_by='ValidationMetricValue',call_me='avg_level') # simple average of top_3 models based on performance in validation
 f.manual_forecast(how='weighted',models=level_models,determine_best_by='ValidationMetricValue',call_me='weighted_level')
 
-f.plot(models='top_5',order_by='LevelTestSetMAPE',print_attr=['LevelTestSetRMSE','LevelTestSetR2','LevelTestSetMAPE','HyperParams','Xvars','models']) # will automatically plot levels for everything
-f.plot_test_set(models='top_5',order_by='LevelTestSetR2',include_train=60) # will automatically plot levels for everything
-f.plot_fitted(models=models + ('avg','weighted'),order_by='TestSetR2') # cannot plot fitted values of all models when levels were different during forecasting
-f.plot_fitted(models=level_models + ('avg_level','weighted_level'),order_by='TestSetR2')
+matplotlib.use('QT5Agg')
+f.plot(models='all',order_by='LevelTestSetMAPE',print_attr=['LevelTestSetRMSE','LevelTestSetR2','LevelTestSetMAPE','HyperParams','Xvars','models']) # will automatically plot levels for everything
+f.plot_test_set(models='all',order_by='LevelTestSetMAPE',include_train=60) # will automatically plot levels for everything
+f.plot_fitted(models=models + ('avg','weighted'),order_by='LevelTestSetMAPE') # cannot plot fitted values of all models when levels were different during forecasting
+f.plot_fitted(models=level_models + ('avg_level','weighted_level'),order_by='LevelTestSetMAPE')
 
 f.export(to_excel=True,determine_best_by='LevelTestSetMAPE',excel_name='housing_different_levels_results.xlsx') # export interesting model metrics and forecasts (both level and non-level)
