8728365c9742c5ed08d35cb05c6e7d0949615ed9
Michael Keith <michaelkeith@utah.gov>
2022-01-25 16:25:40 -0700

    New html structure

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '0.5.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon","autodocsumm","nbsphinx","myst_parser"]
+extensions = ["sphinx.ext.napoleon", "autodocsumm", "nbsphinx", "myst_parser", "sphinxcontrib.confluencebuilder"]
 autodoc_default_options = {"autosummary": True}
 
 # Add any paths that contain templates here, relative to this directory.
