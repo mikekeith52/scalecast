ad50564c486af28441d645d808e721006bb7293d
Michael Keith <michaelkeith@utah.gov>
2022-01-26 11:01:36 -0700

    Reconfig
    Readme disappeared from read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '0.5.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "autodocsumm", "nbsphinx", "myst_parser", "sphinxcontrib.confluencebuilder"]
+extensions = ["sphinx.ext.napoleon", "autodocsumm", "nbsphinx", "myst_parser", "m2r2", "sphinxcontrib.confluencebuilder"]
 autodoc_default_options = {"autosummary": True}
 
 #source_suffix = ['.rst', '.md', '.pdf']
