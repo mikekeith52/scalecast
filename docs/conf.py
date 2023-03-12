0316ecf865d3ce5b7d9cb79f5e83e130a16f87a6
Michael Keith <mikekeith52@gmail.com>
2023-03-12 14:17:00 -0600

    Trying a different version of nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -35,10 +35,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# too lazy to fix errors right now
-nbsphinx_allow_errors = True
-
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
